9d2fce101960e16fd8f08444b9d2b1252953dbef
Quentin Higley <quentinhigley@quentins-air.wireless.weber.edu>
2023-04-26 19:04:04 -0600

    Hi

---
+++ boggle/client/src/components/results_screen.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+import Navbar from "./navbar.js";
+
+import * as functions from "./functions/functions.js";
+
+export default function ResultsScreen() {
+    const [players, setPlayers] = useState([]);
+    const lobby_id = localStorage.getItem("lobby_id");
+    // get the players
+    useEffect(() => {
+        let req = {
+            game_id: lobby_id,
+        };
+        functions.post_call("/game/scores", req, (res) => {
+            console.log(res);
+            setPlayers(res.data.players);
+        });
+    }, []);
+    //create a list of players with their scores order by score
+    const PlayerList = () => {
+        return (
+            <div id="player-list-container">
+                <h2>Players</h2>
+                <ul>
+                    {players.map((player) => {
+                        return (
+                            <div>
+                                <span>{player.username}</span>
+                                <span>{`${player.score}`}</span>
+                                {player.words.map((word) => {
+                                    return <span>{word}</span>;
+                                })}
+                            </div>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    };
+    return (
+        <div
+            id="results-screen-container"
+            className="container"
+        >
+            <Navbar />
+            <div>
+                <PlayerList />
+            </div>
+        </div>
+    );
+}
