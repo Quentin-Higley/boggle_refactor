5fe86f3565d87e8f6ca6cd979befe0fb1b8e332b
Hunter Ferrin <hunterferrin@mail.weber.edu>
2023-04-26 20:02:09 -0600

    added css

--- boggle/client/src/components/navbar.js
+++ boggle/client/src/components/navbar.js
@@ -3,21 +3,24 @@ import axios from "axios";
 
 export default function Navbar() {
     return (
-        <div className="navbar">
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/lobby">Lobby</a>
-                </li>
-                <li>
-                    <a href="/create">Create</a>
-                </li>
-                <li>
-                    <a href="/login">Login</a>
-                </li>
-            </ul>
-        </div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/lobby">Lobby</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/create">Create</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        
     );
 }
